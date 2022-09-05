3f13e865723a09f04bdfc75a89510914938ad866
Dmitriy_Kantserev <dkantserev@gmail.com>
2022-09-06 00:01:55 +0300

    toReview

--- src/main/java/ru/practicum/shareit/requests/controller/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/requests/controller/ItemRequestController.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.requests.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.requests.dto.ItemRequestDto;
 import ru.practicum.shareit.requests.service.RequestService;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
@@ -22,17 +24,20 @@ public class ItemRequestController {
     @PostMapping
     public ItemRequestDto add(@Valid @RequestBody Optional<ItemRequestDto> request,
                               @RequestHeader("X-Sharer-User-Id") Optional<Long> userId) {
+        log.info("add request " + request.orElse(null) + " with userId " + userId.orElse(null));
         return requestService.add(request, userId);
     }
 
     @GetMapping
     public List<ItemRequestDto> get(@RequestHeader("X-Sharer-User-Id") Optional<Long> userId) {
+        log.info("get request  with userId " + userId.orElse(null));
         return requestService.get(userId);
     }
 
     @GetMapping("/{requestId}")
     public ItemRequestDto get(@PathVariable Long requestId,
                               @RequestHeader("X-Sharer-User-Id") Optional<Long> userId) {
+        log.info("get request with requestId " + requestId + "  with userId " + userId.orElse(null));
         return requestService.getForId(requestId, userId);
     }
 
@@ -40,6 +45,8 @@ public class ItemRequestController {
     public List<ItemRequestDto> getAll(@RequestHeader("X-Sharer-User-Id") Optional<Long> userId,
                                        @RequestParam Optional<Long> from,
                                        @RequestParam Optional<Long> size) {
+        log.info("getAll request with userId " + userId.orElse(null) + " with " + from.orElse(null) +
+                " and size " + size.orElse(null));
         return requestService.getAll(userId, from, size);
     }
 
